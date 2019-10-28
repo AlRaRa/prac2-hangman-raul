bf88c6ca493119d7a2659a572b257bdf5becd6f1
Raul Barrios Siles <raulbarriossiles@gmail.com>
2019-10-28 22:40:27 +0100

    feat(service): add getHiddenWord

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,1 +1,31 @@
-export class Service {}
+import { resolve } from 'url';
+
+export class Service {
+  words: string[];
+  wordSelected: string;
+  life: number;
+  letterPressed: string[];
+
+  /*   getWords(): Promise {
+    return new Promise(resolve => resolve);
+  } */
+  initGame(): void {}
+  getRamdomWord(): string {
+    return '';
+  }
+
+  getHiddenWord(): string {
+    const stringLetterRegex = `[^${this.letterPressed.join('')}]`;
+    return this.wordSelected
+      .replace(new RegExp(stringLetterRegex, 'gi'), '_')
+      .split('')
+      .join(' ');
+  }
+  isLetter(letter: string): boolean {
+    return false;
+  }
+
+  checkWin(): boolean {
+    return false;
+  }
+}
