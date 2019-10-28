642778bfc7c0a3f8198cf0f1c3b56e772f4f61a2
Raul Barrios Siles <raulbarriossiles@gmail.com>
2019-10-28 21:32:50 +0100

    test(service): add the service test

--- src/services/service.test.ts
+++ src/services/service.test.ts
@@ -1,13 +1,98 @@
 import { Service } from './service';
 
-const service = new Service();
-describe('#NameFunction', () => {
-  describe('should be true when case', () => {
-    it.each`
-      txt     | expected
-      ${true} | ${true}
-    `(`execute with $txt should be $expected`, ({ txt, expected }) => {
-      expect(txt).toBe(expected);
-    });
+let service = new Service();
+service.wordSelected = 'casa';
+
+describe('#isLetter', () => {
+  describe('should be true', () => {
+    test.each`
+      letter
+      ${'A'}
+      ${'a'}
+      ${'c'}
+    `(
+      `when the letter: "$letter" is in the word ${service.wordSelected}`,
+      ({ letter }) => {
+        expect(service.isLetter(letter)).toBeTruthy();
+      }
+    );
   });
+
+  describe('should be false', () => {
+    test.each`
+      letter
+      ${' '}
+      ${'P'}
+      ${'_'}
+    `(
+      `when the letter: "$letter" isn't in the word: ${service.wordSelected}`,
+      ({ letter }) => {
+        expect(service.isLetter(letter)).toBeTruthy();
+      }
+    );
+  });
+});
+
+describe('#checkWin', () => {
+  describe('should be true', () => {
+    test.each`
+      letters
+      ${['A', 'c', 's']}
+      ${['a', 'c', 's']}
+      ${['A', 'C', 'S']}
+    `(
+      `when the letters array: "$letters" are all the letters of word: ${service.wordSelected}`,
+      ({ letters }) => {
+        service.letterPressed = letters;
+        expect(service.checkWin()).toBeTruthy();
+      }
+    );
+  });
+
+  describe('should be false', () => {
+    test.each`
+      letters
+      ${['A', 'c']}
+      ${['A', 'z']}
+      ${['b', 'z']}
+    `(
+      `when the letters array: "$letters" aren't all the letter of word: ${service.wordSelected}`,
+      ({ letters }) => {
+        service.letterPressed = letters;
+        expect(service.checkWin()).toBeFalsy();
+      }
+    );
+  });
+});
+
+describe('#getHiddenWord', () => {
+  describe('should be well', () => {
+    test.each`
+      letters       | expected
+      ${['A', 'c']} | ${'c a _ a'}
+      ${['A', 'z']} | ${'_ a _ a'}
+      ${['b', 'z']} | ${'_ _ _ _'}
+    `(
+      `when the letter of array: "$letters" that it isn't in the word change to "_" and add " " between letter of "${service.wordSelected}"`,
+      ({ letters, expected }) => {
+        service.letterPressed = letters;
+        expect(service.getHiddenWord()).toEqual(expected);
+      }
+    );
+  });
+
+  /*   describe('should be bad', () => {
+    test.each`
+      letters       | expected
+      ${['A', 'c']} | ${'c a _ a'}
+      ${['A', 'z']} | ${'_ a _ a'}
+      ${['b', 'z']} | ${'_ _ _ _'}
+    `(
+      `when the letter of array: "$letters" that it isn't in the word change to "_" and add " " between letter: ${service.wordSelected}`,
+      ({ letters, expected }) => {
+        service.letterPressed = letters;
+        expect(service.getHiddenWord()).not.toEqual(expected);
+      }
+    );
+  }); */
 });
